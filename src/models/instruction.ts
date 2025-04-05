f315dc68e6bb1b45968307628e983408513c62ce
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-04-05 22:15:34 +0300

    feat instruction types

---
+++ src/models/instruction.ts
@@ -0,0 +1,104 @@
+export namespace instructions32Bits {
+
+interface Instruction {
+    buildInstruction(): bigint;
+}
+
+abstract class InstructionTypeR implements Instruction {
+    protected funct7: number = 0;
+    protected funct3: number = 0;
+    protected rs2: number = 0;
+    protected rs1: number = 0;
+    protected rd: number = 0;
+    protected opcode: number = 0;
+
+    public buildInstruction(): bigint {
+        return ((BigInt(this.funct7) & 0b1111111n) << 25n) |
+            ((BigInt(this.rs2) & 0b11111n) << 20n) |
+            ((BigInt(this.rs1) & 0b11111n) << 15n) |
+            ((BigInt(this.funct3) & 0b111n) << 12n) |
+            ((BigInt(this.rd) & 0b11111n) << 7n) |
+            (BigInt(this.opcode) & 0b1111111n);
+    }
+}
+
+abstract class InstructionTypeI implements Instruction {
+    protected imm: number = 0;
+    protected funct3: number = 0;
+    protected rs1: number = 0;
+    protected rd: number = 0;
+    protected opcode: number = 0;
+
+    public buildInstruction(): bigint {
+        return ((BigInt(this.imm) & 0b111111111111n) << 20n) |
+            ((BigInt(this.rs1) & 0b11111n) << 15n) |
+            ((BigInt(this.funct3) & 0b111n) << 12n) |
+            ((BigInt(this.rd) & 0b11111n) << 7n) |
+            (BigInt(this.opcode) & 0b1111111n);
+    }
+}
+
+abstract class InstructionTypeS implements Instruction {
+    protected imm: number = 0;
+    protected funct3: number = 0;
+    protected rs1: number = 0;
+    protected rs2: number = 0;
+    protected opcode: number = 0;
+
+    public buildInstruction(): bigint {
+        return ((BigInt(this.imm) & 0b111111100000n) << 25n) |
+            ((BigInt(this.rs2) & 0b11111n) << 20n) |
+            ((BigInt(this.rs1) & 0b11111n) << 15n) |
+            ((BigInt(this.funct3) & 0b111n) << 12n) |
+            ((BigInt(this.imm) & 0b11111n) << 7n) |
+            (BigInt(this.opcode) & 0b1111111n);
+    }
+}
+
+abstract class InstructionTypeB extends InstructionTypeS {
+    public buildInstruction(): bigint {
+        let a = (((this.imm >> 12) & 0b1) << 6) | ((this.imm >> 5) & 0b111111);
+        let b = (this.imm & 0b11110) | ((this.imm >> 11) & 0b1);
+
+        return (BigInt(a) << 25n) |
+            ((BigInt(this.rs2) & 0b11111n) << 20n) |
+            ((BigInt(this.rs1) & 0b11111n) << 15n) |
+            ((BigInt(this.funct3) & 0b111n) << 12n) |
+            (BigInt(b) << 7n) |
+            (BigInt(this.opcode) & 0b1111111n);
+    }
+}
+
+abstract class InstructionTypeU implements Instruction {
+    protected imm: number = 0;
+    protected rd: number = 0;
+    protected opcode: number = 0;
+
+    public buildInstruction(): bigint {
+        return ((BigInt(this.imm) & 0b11111111111111111111000000000000n) << 12n) |
+            ((BigInt(this.rd) & 0b11111n) << 7n) |
+            (BigInt(this.opcode) & 0b1111111n);
+    }
+}
+
+abstract class InstructionTypeJ implements Instruction {
+    protected imm: number = 0;
+    protected rd: number = 0;
+    protected opcode: number = 0;
+
+    public buildInstruction(): bigint {
+        let a = (this.imm >> 20) & 0b1;
+        let b = (this.imm >> 1) & 0b1111111111;
+        let c = (this.imm >> 11) & 0b1;
+        let d = (this.imm >> 12) & 0b11111111;
+
+        return ((BigInt(a)) << 31n) |
+            ((BigInt(b)) << 21n) |
+            ((BigInt(c)) << 20n) |
+            ((BigInt(d)) << 12n) |
+            ((BigInt(this.rd) & 0b11111n) << 7n) |
+            (BigInt(this.opcode) & 0b1111111n);
+    }
+}
+}
+
