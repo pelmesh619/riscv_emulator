db567da9c254f3760555a5865ff9d62a0961546f
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-04-05 20:15:32 +0300

    feat test for in-binary conversion

--- tests/word.test.ts
+++ tests/word.test.ts
@@ -64,3 +64,23 @@ describe('Binary to decimal conversion', () => {
         expect(Word.fromSignedBinary(string).getValue()).toBe(-8);
     });
 });
+
+
+describe('Decimal to binary conversion', () => {
+    it.each([
+        [0, '00000000000000000000000000000000', '00000000000000000000000000000000'],
+        [1, '00000000000000000000000000000001', '00000000000000000000000000000001'],
+        [2, '00000000000000000000000000000010', '00000000000000000000000000000010'],
+        [8, '00000000000000000000000000001000', '00000000000000000000000000001000'],
+        [0x7fffffff, '01111111111111111111111111111111', '01111111111111111111111111111111'],
+        [-1, '11111111111111111111111111111111', '-'],
+        [-2, '11111111111111111111111111111110', '-'],
+        [-8, '11111111111111111111111111111000', '-'],
+        [-0x80000000, '10000000000000000000000000000000', '-'],
+    ])('converts %p expecting %p and %p', (value: number, signedBinary: string, unsignedBinary: string) => {
+        expect(new Word(value).asSignedBinary()).toBe(signedBinary);
+        if (value >= 0) {
+            expect(new Word(value).asUnsignedBinary()).toBe(unsignedBinary);
+        }
+    });
+});
