c0a15d5300db1f999c28bbb970c4e49a285bf184
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-04-05 20:19:56 +0300

    refactor tests

--- tests/word.test.ts
+++ tests/word.test.ts
@@ -1,67 +1,19 @@
 import { Word } from '../src/models/basic_types.ts';
 
 describe('Binary to decimal conversion', () => {
-    test('Zero', () => {
-        let string = '00000000000000000000000000000000';
-
-        expect(Word.fromUnsignedBinary(string).getValue()).toBe(0);
-        expect(Word.fromSignedBinary(string).getValue()).toBe(0);
-    });
-
-    test('One', () => {
-        let string = '00000000000000000000000000000001';
-
-        expect(Word.fromUnsignedBinary(string).getValue()).toBe(1);
-        expect(Word.fromSignedBinary(string).getValue()).toBe(1);
-    });
-
-    test('Negative one', () => {
-        let string = '11111111111111111111111111111111';
-
-        expect(Word.fromUnsignedBinary(string).getValue()).toBe(0xffffffff);
-        expect(Word.fromSignedBinary(string).getValue()).toBe(-1);
-    });
-
-    test('Two', () => {
-        let string = '00000000000000000000000000000010';
-
-        expect(Word.fromUnsignedBinary(string).getValue()).toBe(2);
-        expect(Word.fromSignedBinary(string).getValue()).toBe(2);
-    });
-
-    test('Negative two', () => {
-        let string = '11111111111111111111111111111110';
-
-        expect(Word.fromUnsignedBinary(string).getValue()).toBe(0xfffffffe);
-        expect(Word.fromSignedBinary(string).getValue()).toBe(-2);
-    });
-
-    test('2^31', () => {
-        let string = '10000000000000000000000000000000';
-
-        expect(Word.fromUnsignedBinary(string).getValue()).toBe(0x80000000);
-        expect(Word.fromSignedBinary(string).getValue()).toBe(-0x80000000);
-    });
-
-    test('2^31 - 1', () => {
-        let string = '01111111111111111111111111111111';
-
-        expect(Word.fromUnsignedBinary(string).getValue()).toBe(0x7fffffff);
-        expect(Word.fromSignedBinary(string).getValue()).toBe(0x7fffffff);
-    });
-
-    test('Eight', () => {
-        let string = '00000000000000000000000000001000';
-
-        expect(Word.fromUnsignedBinary(string).getValue()).toBe(8);
-        expect(Word.fromSignedBinary(string).getValue()).toBe(8);
-    });
-
-    test('Negative Eight', () => {
-        let string = '11111111111111111111111111111000';
-
-        expect(Word.fromUnsignedBinary(string).getValue()).toBe(0xfffffff8);
-        expect(Word.fromSignedBinary(string).getValue()).toBe(-8);
+    it.each([
+        ['00000000000000000000000000000000', 0, 0],
+        ['00000000000000000000000000000001', 1, 1],
+        ['00000000000000000000000000000010', 2, 2],
+        ['00000000000000000000000000001000', 8, 8],
+        ['11111111111111111111111111111111', -1, 0xffffffff],
+        ['11111111111111111111111111111110', -2, 0xfffffffe],
+        ['11111111111111111111111111111000', -8, 0xfffffff8],
+        ['10000000000000000000000000000000', -0x80000000, 0x80000000],
+        ['01111111111111111111111111111111', 0x7fffffff, 0x7fffffff],
+    ])('converts %p expecting %p and %p', (binary: string, signedNumber: number, unsignedNumber: number) => {
+        expect(Word.fromUnsignedBinary(binary).getValue()).toBe(unsignedNumber);
+        expect(Word.fromSignedBinary(binary).getValue()).toBe(signedNumber);
     });
 });
 
