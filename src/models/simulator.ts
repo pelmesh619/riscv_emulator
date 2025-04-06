71b8c0aa33cfe807949402241435e3f1f57b6faa
pelmeshke <75571195+pelmesh619@users.noreply.github.com>
2025-04-06 03:43:59 +0300

    refactor program counter

--- src/models/simulator.ts
+++ src/models/simulator.ts
@@ -1,4 +1,5 @@
 import { Word } from './basic_types.js'
+import { getRegisterIndex } from '../utilities.js'
 
 export class RegisterContext {
     private registers: Word[] = [
@@ -6,11 +7,21 @@ export class RegisterContext {
         Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero,
         Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero,
         Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero,
-        Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero,
+        Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero, Word.Zero
     ]
+
+    private programCounter = Word.Zero;
+
+    private _programCounterIndex = getRegisterIndex('pc');
+
+    public get programCounterIndex() { return this._programCounterIndex }
 
     public setRegister(index: number, value: Word) {
         if (index == 0) {
+            return;
+        }
+        if (index == this._programCounterIndex) {
+            this.programCounter = value;
             return;
         }
         this.registers[index] = value;
@@ -19,6 +30,9 @@ export class RegisterContext {
     public getRegister(index: number) {
         if (index == 0) {
             return Word.Zero;
+        }
+        if (index == this._programCounterIndex) {
+            return this.programCounter;
         }
         return this.registers[index];
     }
